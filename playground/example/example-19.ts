97f2a5aedeecf50be68a1d2a32f67df699c38563
gongzhen <517441540@qq.com>
2023-08-29 15:17:05 +0800

    feat: cutout handle base capacity

--- playground/example/example-19.ts
+++ playground/example/example-19.ts
@@ -4,24 +4,26 @@ import { BoxGeometry, Scene, MeshBasicMaterial, Mesh, PerspectiveCamera } from '
  * example-19
  * cut scene   
  */
-
 const scene = new Scene({
-    orbitControls: true
+    orbitControls: true,
+    cutout: false,
 })
-scene.render(document.querySelector('#app')!)
-scene.startFrameAnimate()
 
+const minCamera = new PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 5000)
+scene.cutoutCamera = minCamera
+scene.cutoutArea = { x: 0, y: 0, width: 200, height: 200 }
 
 const geometry = new BoxGeometry(2, 2, 2)
 const material = new MeshBasicMaterial({ color: 0x00FF00 })
 const box = new Mesh(geometry, material)
-// box.position.set(0, 0, 0)
 
-
-// const minCamera = new PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 5000)
-// // scene.cutout = true
-// scene.cutoutCamera = minCamera
-// scene.cutoutArea = { x: 0, y: 0, z: 10 }
-
+scene.add(minCamera)
 scene.add(box)
+
+scene.render(document.querySelector('#app')!)
+scene.startFrameAnimate()
+
+
+
+
 
