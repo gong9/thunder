cf74481a98ae71af2fab98549a3b54aaf6086cd5
gongzhen <517441540@qq.com>
2024-03-30 17:57:48 +0800

    feat: init terrain

---
+++ packages/thunder-terrain/.fatherrc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'father';
+import path from 'path'
+
+export default defineConfig({
+  extends: path.resolve(__dirname,'../../.fatherrc'),
+  umd: { output:'lib'},
+  
+});
---
+++ packages/thunder-terrain/README.md
@@ -0,0 +1,4 @@
+# thunder-terrain
+
+thunder-terrain
+
